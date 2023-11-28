aafb96e524b48f3c4ad55ab2a9c00a4f366649d5
elisiolg <elisiolg@gmail.com>
2023-11-28 20:47:13 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    modified:   src/components/brand/Brand.jsx
    modified:   src/components/brand/brand.css
    new file:   src/components/brand/imports.js
    modified:   src/components/feature/Feature.jsx
    modified:   src/components/feature/feature.css
    modified:   src/containers/features/Features.jsx
    modified:   src/containers/features/features.css
    modified:   src/containers/possibility/Possibility.jsx
    modified:   src/containers/possibility/possibility.css
    modified:   src/containers/whatGPT3/WhatGPT3.jsx
    modified:   src/containers/whatGPT3/whatgpt3.css

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './brand.css'
+import { google, dropbox, slack, atlassian, shopify } from './imports'
 
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='gpt3__brand section_padding'>
+      <div>
+        <img src={google} alt='Google' />
+      </div>
+      <div>
+        <img src={slack} alt='Slack' />
+      </div>
+      <div>
+        <img src={atlassian} alt='Atlassian' />
+      </div>
+      <div>
+        <img src={dropbox} alt='Dropbox' />
+      </div>
+      <div>
+        <img src={shopify} alt='Shopify' />
+      </div>
+    </div>
   )
 }
 
